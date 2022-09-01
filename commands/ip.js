8d1248207a26b2e6b78e4391682fbfbca5c45b3e
Jonas Pohl <jonas.jp4c@gmail.com>
2022-09-01 21:32:48 +0200

    Dockerize

--- commands/ip.js
+++ commands/ip.js
@@ -1,8 +1,6 @@
 module.exports = function (msg, args) {
     const http = require('http');
     http.get({ 'host': 'api.ipify.org', 'port': 80, 'path': '/' }, function (resp) {
-        resp.on('data', function (ip) {
-            msg.channel.send(ip + "");
-        });
+        resp.on('data', ip => msg.channel.send(ip + ""));
     });
 }
--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 require('dotenv').config();
 
+if(process.env.BOTTOKEN == "" || process.env.BOTTOKEN == undefined) {
+    console.log("Please set BOTTOKEN as environment variable");
+    process.exit(1);
+}
+if(process.env.SERVER_ID == "" || process.env.SERVER_ID == undefined) {
+    console.log("Please set SERVER_ID as environment variable");
+    process.exit(1);
+}
+if(process.env.CHANNEL_ID == "" || process.env.CHANNEL_ID == undefined) {
+    console.log("Please set CHANNEL_ID as environment variable");
+    process.exit(1);
+}
+
+
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 client.login(process.env.BOTTOKEN);
