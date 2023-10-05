1b89390f02c4251adee6e8e6dc9ca80eb30875f4
Tilman Holube <tilman@holube.de>
2023-10-05 14:08:04 +0200

    ports

--- commands/ip.js
+++ commands/ip.js
@@ -17,8 +17,21 @@ async function getIP(interaction) {
     console.log('getting ip');
     http.get({ 'host': 'api.ipify.org', 'port': 80, 'path': '/' }, function (resp) {
         resp.on('data', async ip => {
+            let output = `The current ip is: ${ip}`;
+
+            if (!(process.env.SERVER_PORT == undefined || process.env.SERVER_PORT == "")) {
+                output += `\nServer: ${ip}:${process.env.SERVER_PORT}`
+            }
+            if (!(process.env.MAP_PORT == undefined || process.env.MAP_PORT == "")) {
+                let s = "";
+                if (!(process.env.MAP_HTTPS == undefined || process.env.MAPMAP_HTTPS_PORT == "") && process.env.MAP_HTTPS == "true") {
+                    s = "s";
+                }
+                output += `\nMap: http${s}://${ip}:${process.env.MAP_PORT}`
+            }
+
             console.log(`ip: ${ip}`);
-            await interaction.editReply(`The current ip is: ${ip}`);
+            await interaction.editReply(output);
         });
     });
 }
