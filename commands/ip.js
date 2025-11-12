51aa3798abb12dd3ba282a17c1739824c3d4e677
Tilman Holube <tilman@holube.de>
2025-11-13 00:03:56 +0100

    Add back IP output
    - Adds back the IP output accidentally removed in JayPi4c/MinecraftDiscordIPBot#6
    - Adds error handling to IP requests

--- commands/ip.js
+++ commands/ip.js
@@ -21,17 +21,23 @@ module.exports = {
 async function getIP(interaction) {
     console.log('getting ip');
 
-    let output = display_mg.get() + `\n`;
+    let output = display_mg.get();
 
     if (enable_ipv4 === "true") {
         let v4 = await getIPv4();
         console.log(`ipv4: ${v4}`);
-        output = addVersion(output, v4);
+        if ("IPv4 Unavailable" !== v4) {
+            output += `\nIPv4: ${v4}`;
+            output = addVersion(output, v4);
+        }
     }
     if (enable_ipv6 === "true") {
         let v6 = await getIPv6();
         console.log(`ipv6: ${v6}`);
-        output = addVersion(output, "[" + v6 + "]");
+        if ("IPv6 Unavailable" !== v6) {
+            output += `\nIPv6: ${v6}`;
+            output = addVersion(output, "[" + v6 + "]");
+        }
     }
 
     await interaction.editReply(output);
@@ -55,9 +61,17 @@ function addVersion(output, ip) {
 async function getIPv4() {
     return fetch("https://api.ipify.org")
         .then(response => response.text())
+        .catch(error_ => {
+            console.log(`Failed to get IPv4: ${error_}`);
+            return "IPv4 Unavailable";
+        });
 }
 
 async function getIPv6() {
     return fetch("https://api6.ipify.org")
         .then(response => response.text())
+        .catch(error_ => {
+            console.log(`Failed to get IPv6: ${error_}`);
+            return "IPv6 Unavailable";
+        });
 }
